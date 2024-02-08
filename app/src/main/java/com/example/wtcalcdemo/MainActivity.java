4ac51487dd1a8b10bc1da24a7ec85dad78ff953e
Gutierrez Guisao <gutierrezguisaod@student.douglascollege.ca>
2024-02-08 10:26:32 -0800

    Comment added

--- app/src/main/java/com/example/wtcalcdemo/MainActivity.java
+++ app/src/main/java/com/example/wtcalcdemo/MainActivity.java
@@ -16,6 +16,8 @@ import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity {
     TextView txtViewResults;
 
+    //Added another line on local on feb 8th
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
